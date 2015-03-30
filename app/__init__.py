fae17fa99ea967e9247d42a57076bf835a9fa2a6
Viviana Terceros <vivitc@gmail.com>
2015-03-30 00:22:07 -0300

    first version of the template

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
